c128d13aa6e32c8f3abb1a401d175bb75cf71069
Sam Koscelny <143337381+sam-kos41@users.noreply.github.com>
2024-10-21 15:39:32 -0400

    Update cluster_map.tsx

--- app/cluster_map.tsx
+++ app/cluster_map.tsx
@@ -1,4 +1,4 @@
-"use client"; 
+"use client";
 
 import { useEffect } from 'react';
 
@@ -9,15 +9,19 @@ const ClusterMap = () => {
     script.src = '//cdn.clustrmaps.com/map_v2.js?cl=ffffff&w=a&t=tt&d=6x990Sq4KN-2HIkbEshZK88GBdjibt2WZMBVfkOwIRY&cmo=3f403f&ct=3f403f&co=9fb8ad&cmn=e5d8b2';
     script.async = true;
     document.body.appendChild(script);
-    
+
+    // Clean up the script when component unmounts
     return () => {
-      document.body.removeChild(script); // Cleanup script on unmount
+      const existingScript = document.getElementById('clustrmaps');
+      if (existingScript) {
+        document.body.removeChild(existingScript);
+      }
     };
   }, []);
 
   return (
     <div id="clustrmaps-widget" className="w-full flex justify-center mt-8">
-      {/* ClustrMaps map will render here */}
+      {/* The map will render here */}
     </div>
   );
 };
