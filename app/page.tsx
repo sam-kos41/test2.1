b9edc0dfd15873aa8f366f9a6e7e698e40fe8e4e
Sam Koscelny <143337381+sam-kos41@users.noreply.github.com>
2024-10-21 15:30:26 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,12 @@
-"use client";
+"use client"; 
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
-import Link from 'next/link'; 
-import { FaFileAlt } from 'react-icons/fa'; 
+import Link from 'next/link'; // Import Link for internal routing
+import { FaFileAlt } from 'react-icons/fa'; // Import icon for the thesis link
 
 const HomePage = () => {
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
-
-  // Use useEffect to inject the ClustrMaps script when the component mounts
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.id = 'clustrmaps';
-    script.src = '//cdn.clustrmaps.com/map_v2.js?cl=ffffff&w=a&t=tt&d=6x990Sq4KN-2HIkbEshZK88GBdjibt2WZMBVfkOwIRY&cmo=3f403f&ct=3f403f&co=9fb8ad&cmn=e5d8b2';
-    script.async = true;
-    document.body.appendChild(script);
-    
-    return () => {
-      // Cleanup script if the component is unmounted
-      document.body.removeChild(script);
-    };
-  }, []);
 
   const toggleAccordion = (index: number) => {
     setActiveIndex(activeIndex === index ? null : index);
@@ -34,8 +20,7 @@ const HomePage = () => {
           {/* Name Section */}
           <h1 className="text-4xl font-bold">Samuel N. Koscelny</h1>
 
-
-            {/* Navigation Links */}
+          {/* Navigation Links */}
           <div className="mt-4 space-x-12 font-sans text-lg flex justify-center">
             <Link href="/" passHref>
               <a className="hover:text-[#2541B2] hover:underline">Home</a>
@@ -184,16 +169,6 @@ const HomePage = () => {
         </div>
       </section>
 
-      {/* Add ClustrMaps Section */}
-      <section className="bg-white py-16">
-        <div className="container mx-auto text-center">
-          <h2 className="text-3xl font-bold mb-4">Website Traffic Map</h2>
-          <div id="clustrmaps-widget" className="w-full flex justify-center">
-            {/* ClustrMaps widget will load here */}
-          </div>
-        </div>
-      </section>
-
       {/* Footer */}
       <footer className="bg-[#A8AB9B] text-black py-4 text-center">
         <p>&copy; Created & designed by Sam Koscelny</p>
@@ -203,3 +178,4 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
